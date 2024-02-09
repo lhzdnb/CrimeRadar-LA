5700c7968fb6ed5f7940a058e3ef1039acacf04d
lh20020727 <lh20020727@sjtu.edu.cn>
2024-02-09 00:38:40 -0800

    configure router

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+// react 自带库
+import React from "react";
+
+// 第三方库
+import { Button, Form, Input } from "antd";
+
+// 自己写的
+import "./index.css";
+import LoginForm from "./LoginForm";
 
 function Login(props) {
   return (
-    <div></div>
+    <div className={"login_page"}>
+      <LoginForm />
+    </div>
   );
 }
 
