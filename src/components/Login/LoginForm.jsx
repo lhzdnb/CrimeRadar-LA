1809108c7768264d01fd7db5bb9eaea079d08bfb
lh20020727 <lh20020727@sjtu.edu.cn>
2024-02-09 16:34:23 -0800

    finish the login page

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,64 +1,93 @@
-import React from "react";
-import { Button, Form, Input, message } from "antd";
+import React, { useState } from "react";
+import { Button, Checkbox, Flex, Form, Input, message } from "antd";
+import {
+  EyeInvisibleOutlined,
+  EyeOutlined,
+  LockOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { verifyUserCredentialApi } from "./loginApi";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm(props) {
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const verifyCredentials = async (values) => {
+    setLoading(true);
     const token = await verifyUserCredentialApi(values);
     if (!token) {
+      setLoading(false);
       return message.error("登录失败，请检查用户名和密码");
     }
+    setLoading(false);
     localStorage.setItem("token", token);
     navigate("/");
   };
 
   return (
-    <div>
-      <Form
-        className={"login_form"}
-        onFinish={verifyCredentials}
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
+    <Form
+      className={"login_form"}
+      onFinish={verifyCredentials}
+      initialValues={{
+        remember: true,
+      }}
+    >
+      <Form.Item
+        name={"username"}
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
       >
-        <Form.Item
-          name={"username"}
-          label={"Username"}
-          rules={[
-            {
-              required: true,
-              message: "Please input your username!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <Input
+          prefix={<UserOutlined />}
+          className={"login_input"}
+          placeholder={"Username"}
+        />
+      </Form.Item>
 
-        <Form.Item
-          name={"password"}
-          label={"Password"}
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password
+          prefix={<LockOutlined />}
+          className={"login_input"}
+          placeholder={"Password"}
+        />
+      </Form.Item>
 
-        <Form.Item>
-          <Button htmlType={"submit"} type="primary" danger={true}>
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox className="remember_me" checked>
+          Remember me
+        </Checkbox>
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          htmlType={"submit"}
+          type="primary"
+          danger={true}
+          loading={loading}
+          className={"login_button"}
+        >
+          {loading ? "登录中..." : "登录"}
+        </Button>
+        <Button type={"link"} className={"register-button"}>
+          现在注册
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
 
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,17 +2,51 @@
 import React from "react";
 
 // 第三方库
-import { Button, Form, Input } from "antd";
 
 // 自己写的
 import "./index.css";
 import LoginForm from "./LoginForm";
+import { Button, Flex } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Typography } from "antd";
+const { Title } = Typography;
 
 function Login(props) {
+  const navigate = useNavigate();
+  function toRegister() {
+    navigate("/register");
+  }
+  function toAbout() {
+    navigate("/about");
+  }
+
   return (
-    <div className={"login_page"}>
+    <Flex
+      className={"login_page"}
+      vertical
+      gap={"large"}
+      align={"center"}
+      justify={"center"}
+    >
+      <Flex
+        gap={"middle"}
+        justify={"space-between"}
+        align={"center"}
+        className={"nav-bar"}
+      >
+        <div>Logo</div>
+        <Flex gap={"middle"}>
+          <Button type={"link"} className={"link"} onClick={toRegister}>
+            注册
+          </Button>
+          <Button type={"link"} className={"link"} onClick={toAbout}>
+            关于我们
+          </Button>
+        </Flex>
+      </Flex>
+      <h1 className={"title"}>CrimeRadar - LA</h1>
       <LoginForm />
-    </div>
+    </Flex>
   );
 }
 
